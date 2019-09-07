4bad694f4e87a73fc813606d1cf2d7705b4a3236
lb <lb@esunadv.com>
2019-09-07 16:15:50 +0800

    lb

--- ailis/database/migrations/2014_10_12_000000_create_users_table.php
+++ ailis/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,12 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->bigIncrements('uuid');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('user')->unique()
             $table->string('password');
-            $table->rememberToken();
+            $table->string('region');
+            $table->string('other');   
             $table->timestamps();
         });
     }
